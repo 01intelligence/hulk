88d37c544984114fa0ae22e8c205900a38a764ce
RideWindX <ridewindx@gmail.com>
2021-06-09 20:51:17 +0800

    Update mod config::storageclass

--- src/config/storageclass/mod.rs
+++ src/config/storageclass/mod.rs
@@ -1,3 +1,9 @@
+use anyhow::{anyhow, bail};
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+
+use super::config::{KV, KVS};
+
 // Reduced redundancy storage class
 pub const RRS: &str = "REDUCED_REDUNDANCY";
 // Standard storage class
@@ -24,10 +30,143 @@ pub const DMA_ENV: &str = "MINIO_STORAGE_CLASS_DMA";
 const SCHEME_PREFIX: &str = "EC";
 
 // Min parity disks
-const MIN_PARITY_DISKS: usize = 2;
+const MIN_PARITY_DISKS: u8 = 2;
 
 // Default RRS parity is always minimum parity.
-const DEFAULT_RRS_PARITY: usize = MIN_PARITY_DISKS;
+const DEFAULT_RRS_PARITY: u8 = MIN_PARITY_DISKS;
 
 // Default DMA value
 const DEFAULT_DMA: &str = DMA_READ_WRITE;
+
+lazy_static! {
+    // Default storage class config
+    pub static ref DEFAULT_KVS: KVS = KVS(vec![
+        KV {
+            key: CLASS_STANDARD.to_owned(),
+            value: "".to_owned(),
+        },
+        KV {
+            key: CLASS_RRS.to_owned(),
+            value: "EC:2".to_owned(),
+        },
+        KV {
+            key: CLASS_DMA.to_owned(),
+            value: DEFAULT_DMA.to_owned(),
+        },
+    ]);
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct StorageClass {
+    pub parity: u8,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub standard: StorageClass,
+    pub rrs: StorageClass,
+    pub dma: String,
+}
+
+pub fn is_valid(sc: &str) -> bool {
+    sc == RRS || sc == STANDARD
+}
+
+impl ToString for StorageClass {
+    fn to_string(&self) -> String {
+        if self.parity != 0 {
+            format!("{}:{}", SCHEME_PREFIX, self.parity)
+        } else {
+            "".to_owned()
+        }
+    }
+}
+
+impl Config {
+    // Returns the data and parity drive count based on storage class
+    // If storage class is set using the env vars HULK_STORAGE_CLASS_RRS and
+    // HULK_STORAGE_CLASS_STANDARD or server config fields corresponding values are
+    // returned.
+    //
+    // -- if input storage class is empty then standard is assumed
+    // -- if input is RRS but RRS is not configured default '2' parity
+    //    for RRS is assumed
+    // -- if input is STANDARD but STANDARD is not configured '0' parity
+    //    is returned, the caller is expected to choose the right parity
+    //    at that point.
+    pub fn get_parity_for_sc(&self, sc: &str) -> u8 {
+        if sc.trim() == RRS {
+            // Set the RRS parity if available
+            if self.rrs.parity == 0 {
+                DEFAULT_RRS_PARITY
+            } else {
+                self.rrs.parity
+            }
+        } else {
+            self.standard.parity
+        }
+    }
+}
+
+// Parses given env string and returns a storageClass structure.
+// Supported Storage Class format is "Scheme:Number of parity disks".
+// Currently only supported scheme is "EC".
+fn parse_storage_class(sc: &str) -> anyhow::Result<StorageClass> {
+    let s: Vec<&str> = sc.split(':').collect();
+    if s.len() > 2 {
+        bail!("too many sections in '{}'", sc);
+    } else if s.len() < 2 {
+        bail!("too few sections in '{}'", sc);
+    }
+
+    // Only allowed scheme is "EC".
+    if s[0] != SCHEME_PREFIX {
+        bail!("supported scheme is 'EC', but not '{}'", s[0]);
+    }
+
+    let parity = s[1]
+        .parse::<u8>()
+        .map_err(|_| anyhow!("invalid parity '{}'", s[1]))?;
+
+    Ok(StorageClass { parity })
+}
+
+// Validates the parity disks.
+pub fn validate_parity(ss_parity: u8, rrs_parity: u8, set_drive_count: u8) -> anyhow::Result<()> {
+    // SS parity disks should be greater than or equal to minParityDisks.
+    // Parity below minParityDisks is not supported.
+    if ss_parity > 0 && ss_parity < MIN_PARITY_DISKS {
+        bail!(
+            "Standard storage class parity {} should be greater than or equal to {}",
+            ss_parity,
+            MIN_PARITY_DISKS
+        );
+    }
+    // RRS parity disks should be greater than or equal to minParityDisks.
+    // Parity below minParityDisks is not supported.
+    if rrs_parity > 0 && rrs_parity < MIN_PARITY_DISKS {
+        bail!(
+            "Reduced redundancy storage class parity {} should be greater than or equal to {}",
+            rrs_parity,
+            MIN_PARITY_DISKS
+        );
+    }
+    if ss_parity > set_drive_count / 2 {
+        bail!(
+            "Standard storage class parity {} should be less than or equal to {}",
+            ss_parity,
+            set_drive_count / 2
+        );
+    }
+    if rrs_parity > set_drive_count / 2 {
+        bail!(
+            "Reduced redundancy storage class parity {} should be less than or equal to {}",
+            rrs_parity,
+            set_drive_count
+        );
+    }
+    if ss_parity > 0 && rrs_parity > 0 && ss_parity < rrs_parity {
+        bail!("Standard storage class parity disks {} should be greater than or equal to Reduced redundancy storage class parity disks {}", ss_parity, rrs_parity);
+    }
+    Ok(())
+}

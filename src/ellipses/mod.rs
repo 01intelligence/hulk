8b97957a7533c40dfc643f04d921279945105309
RideWindX <ridewindx@gmail.com>
2021-06-15 19:49:37 +0800

    Draft mod ellipses 2nd

--- src/ellipses/mod.rs
+++ src/ellipses/mod.rs
@@ -1,10 +1,10 @@
-use anyhow::bail;
+use anyhow::{anyhow, bail};
 use lazy_static::lazy_static;
 use regex::Regex;
 
 lazy_static! {
     // Regex to extract ellipses syntax inputs.
-    static ref regex_ellipses: Regex =
+    static ref REGEX_ELLIPSES: Regex =
         Regex::new(r#"(.*)({[0-9a-z]*\.\.\.[0-9a-z]*})(.*)"#).unwrap();
 }
 
@@ -36,6 +36,7 @@ impl ArgPattern {
 }
 
 impl Pattern {
+    // Expands a ellipses pattern.
     pub fn expand(&self) -> Vec<String> {
         let mut labels = Vec::with_capacity(self.seq.len());
         for s in &self.seq {
@@ -53,6 +54,9 @@ impl Pattern {
     }
 }
 
+// Parses an ellipses range pattern of following style
+// `{1...64}`
+// `{33...64}`
 fn parse_ellipses_range(mut pattern: &str) -> anyhow::Result<Vec<String>> {
     if !pattern.contains(OPEN_BRACES) || !pattern.contains(CLOSE_BRACES) {
         bail!("invalid argument");
@@ -85,11 +89,28 @@ fn parse_ellipses_range(mut pattern: &str) -> anyhow::Result<Vec<String>> {
         )
     }
 
-    let seq = Vec::new();
-    for i in start..=end {}
+    let mut seq = Vec::new();
+    for i in start..=end {
+        if ellipses_range[0].starts_with('0') && ellipses_range[0].len() > 1
+            || ellipses_range[1].starts_with('0')
+        {
+            if hexadecimal {
+                seq.push(format!("{:0width$x}", i, width = ellipses_range[1].len()));
+            } else {
+                seq.push(format!("{:0width$}", i, width = ellipses_range[1].len()));
+            }
+        } else {
+            if hexadecimal {
+                seq.push(format!("{:x}", i));
+            } else {
+                seq.push(format!("{}", i));
+            }
+        }
+    }
     Ok(seq)
 }
 
+// Recursively expands labels into its respective forms.
 fn arg_expander(labels: &[Vec<String>]) -> Vec<Vec<String>> {
     let mut out: Vec<Vec<String>> = Vec::new();
     if labels.len() == 1 {
@@ -111,4 +132,62 @@ fn arg_expander(labels: &[Vec<String>]) -> Vec<Vec<String>> {
 pub fn has_ellipses(args: &[&str]) -> bool {
     args.iter()
         .all(|&a| a.contains(ELLIPSES) || (a.contains(OPEN_BRACES) && a.contains(CLOSE_BRACES)))
+}
+
+fn err_invalid_ellipses_format_fn(arg: &str) -> anyhow::Error {
+    anyhow!("Invalid ellipsis format in '{}', Ellipsis range must be provided in format {{N...M}} where N and M are positive integers, M must be greater than N,  with an allowed minimum range of 4", arg)
+}
+
+// Finds all ellipses patterns, recursively and parses the ranges numerically.
+pub fn find_ellipses_patterns(arg: &str) -> anyhow::Result<ArgPattern> {
+    // We throw an error if arg doesn't have any recognizable ellipses pattern.
+    let mut parts: Vec<&str> = REGEX_ELLIPSES
+        .captures(arg)
+        .ok_or(err_invalid_ellipses_format_fn(arg))?
+        .iter()
+        .map(|v| v.unwrap().as_str())
+        .collect();
+    parts = parts.into_iter().skip(1).collect();
+
+    let mut patterns: Vec<Pattern> = Vec::new();
+    let mut pattern_found = REGEX_ELLIPSES.is_match(parts[0]);
+    while pattern_found {
+        let seq = parse_ellipses_range(parts[1])?;
+        patterns.push(Pattern {
+            prefix: "".to_owned(),
+            suffix: parts[2].to_owned(),
+            seq,
+        });
+        if let Some(mut p) = REGEX_ELLIPSES.captures(parts[0]) {
+            parts = p.iter().map(|v| v.unwrap().as_str()).collect();
+            parts = parts.into_iter().skip(1).collect();
+            pattern_found = has_ellipses(&vec![parts[0]]);
+            continue;
+        }
+        break;
+    }
+
+    if parts.len() > 0 {
+        let seq = parse_ellipses_range(parts[1])?;
+        patterns.push(Pattern {
+            prefix: parts[0].to_owned(),
+            suffix: parts[2].to_owned(),
+            seq,
+        })
+    }
+
+    // Check if any of the prefix or suffixes now have flower braces
+    // left over, in such a case we generally think that there is
+    // perhaps a typo in users input and error out accordingly.
+    for pattern in &patterns {
+        if pattern.prefix.contains(OPEN_BRACES)
+            || pattern.prefix.contains(CLOSE_BRACES)
+            || pattern.suffix.contains(OPEN_BRACES)
+            || pattern.suffix.contains(CLOSE_BRACES)
+        {
+            return Err(err_invalid_ellipses_format_fn(arg));
+        }
+    }
+
+    Ok(ArgPattern(patterns))
 }

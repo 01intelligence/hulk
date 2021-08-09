76547ad50f1907ac48a694b7aa7366595d07b26a
Nelson Liang <nelson_liang@163.com>
2021-08-09 20:28:36 +0800

    Add bucket::policy::id test

--- src/bucket/policy/id.rs
+++ src/bucket/policy/id.rs
@@ -6,3 +6,28 @@ impl super::Valid for ID {
         std::str::from_utf8(self.as_bytes()).is_ok()
     }
 }
+
+mod tests {
+    use super::super::Valid;
+    use super::*;
+
+    #[test]
+    fn test_id_is_valid() {
+        let cases = [
+            (ID::from("DenyEncryptionSt1"), true),
+            (ID::from(""), true),
+            (unsafe { ID::from_utf8_unchecked(vec![b'a', b'a', b'\xe2']) }, false),
+        ];
+
+        for (key, expected_result) in cases.iter() {
+            assert_eq!(
+                key.is_valid(),
+                *expected_result,
+                "key: '{:?}', expected: {}, got: {}",
+                key,
+                expected_result,
+                key.is_valid()
+            );
+        }
+    }
+}

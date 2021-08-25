18cd6519998fbd2a84d767a3b25e88794f6812db
He Weidong <67892702+zlhwdsz@users.noreply.github.com>
2021-08-25 21:19:41 +0800

    Test trie (#31)
    * fix and test trie::insert
    
    * add trie::tests::test_prefix_match
    
    * use Trie::default()

--- src/trie/mod.rs
+++ src/trie/mod.rs
@@ -25,7 +25,7 @@ impl Trie {
     pub fn insert(&mut self, key: String) {
         let mut cur_node = &mut self.root;
         for v in key.chars() {
-            if cur_node.child.contains_key(&v) {
+            if !cur_node.child.contains_key(&v) {
                 cur_node.child.insert(v, Node::default());
             }
             cur_node = cur_node.child.get_mut(&v).unwrap();
@@ -85,5 +85,49 @@ impl Trie {
             index = key.chars().count();
         }
         Some((cur_node, index))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    // Ensure that we can insert new keys into the tree, then check the size.
+    fn test_trie_insert() {
+        let mut trie = Trie::default();
+
+        trie.insert("key".to_string());
+        trie.insert("keyy".to_string());
+
+        // After inserting, we should have a size of two.
+        assert_eq!(trie.size, 2, "expected size 2, got: {}", trie.size)
+    }
+
+    #[test]
+    // Ensure that prefix_match gives us the correct two keys in the tree.
+    fn test_trie_prefix_match() {
+        let mut trie = Trie::default();
+
+        // Feed it some fodder: only "minio" and "miny-o's" should trip the matcher.
+        trie.insert("minio".to_string());
+        trie.insert("amazon".to_string());
+        trie.insert("cheerio".to_string());
+        trie.insert("miny-o's".to_string());
+
+        let matches = trie.prefix_match("min");
+        assert_eq!(
+            matches.len(),
+            2,
+            "expected two matches, got: {}",
+            matches.len()
+        );
+
+        if matches[0] != "minio" && matches[1] != "minio" {
+            panic!(
+                "expected one match to be 'minio', got: '{}' and '{}'",
+                matches[0], matches[1]
+            )
+        }
     }
 }

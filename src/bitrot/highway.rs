7299de242cfa1f1610a3740efb3c4d5be79ffb7c
RideWindX <ridewindx@gmail.com>
2021-08-27 18:43:39 +0800

    chore: update bitrot

--- src/bitrot/highway.rs
+++ src/bitrot/highway.rs
@@ -6,15 +6,10 @@ use futures_util::ready;
 use highway::{HighwayHash, HighwayHasher, Key};
 use tokio::io::{AsyncBufRead, AsyncWrite, AsyncWriteExt};
 
+use crate::storage::StorageApi;
+
 const MAGIC_HIGHWAY_HASH_256_KEY: &[u8; 32] =
      b"\x4b\xe7\x34\xfa\x8e\x23\x8a\xcd\x26\x3e\x83\xe6\xbb\x96\x85\x52\x04\x0f\x93\x5d\xa3\x9f\x44\x14\x97\xe0\x9d\x13\x22\xde\x36\xa0";
-
-#[pin_project::pin_project]
-pub struct HighwayBitrotWriter<T: AsyncWrite + Unpin> {
-    #[pin]
-    writer: T,
-    hasher: HighwayHasher,
-}
 
 fn high_way_hasher() -> HighwayHasher {
     let key =
@@ -22,22 +17,92 @@ fn high_way_hasher() -> HighwayHasher {
     HighwayHasher::new(Key(key))
 }
 
-impl<T: AsyncWrite + Unpin> HighwayBitrotWriter<T> {
-    pub fn new(writer: T, shard_size: u64) -> Self {
+#[pin_project::pin_project]
+pub struct HighwayBitrotWriter {
+    #[pin]
+    writer: Box<dyn AsyncWrite + Unpin>,
+    hasher: HighwayHasher,
+    state: State,
+}
+
+enum State {
+    Start,
+    PollHash([u8; 32], usize),
+    PollContent,
+}
+
+impl HighwayBitrotWriter {
+    pub fn new(writer: Box<dyn AsyncWrite + Unpin>) -> Self {
         let hasher = high_way_hasher();
-        Self { writer, hasher }
+        Self {
+            writer,
+            hasher,
+            state: State::Start,
+        }
     }
 
-    pub fn new_with_storage_api() -> Self {
+    pub async fn new_with_storage_api(
+        storage: StorageApi,
+        volume: &str,
+        path: &str,
+        length: Option<u64>,
+        shard_size: u64,
+    ) -> anyhow::Result<Self> {
+        let mut total_size = None;
+        if let Some(length) = length {
+            let bitrot_sums_size = (crate::utils::ceil_frac(length as i64, shard_size as i64)
+                as u64)
+                * (std::mem::size_of::<[u64; 4]>() as u64);
+            total_size = Some(bitrot_sums_size + length);
+        }
+        let writer = storage.create_file(volume, path, total_size).await?;
+        let hasher = high_way_hasher();
+        Ok(Self {
+            writer,
+            hasher,
+            state: State::Start,
+        })
+    }
+}
+
+impl AsyncWrite for HighwayBitrotWriter {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<std::io::Result<usize>> {
+        loop {
+            let this = self.as_mut().project();
+            match this.state {
+                State::Start => {
+                    let hasher = this.hasher.clone(); // TODO: avoid clone?
+                    let hash = hasher.hash256(buf);
+                    let hash = unsafe { std::mem::transmute::<[u64; 4], [u8; 32]>(hash) };
+                    *this.state = State::PollHash(hash, 0);
+                }
+                State::PollHash(ref hash, ref mut written) => {
+                    let buf = &hash[*written..];
+                    let n = ready!(this.writer.poll_write(cx, buf))?;
+                    *written += n;
+                    if n == 0 {
+                        // no longer able to write
+                        return Poll::Ready(Ok(0));
+                    } else if *written == hash.len() {
+                        *this.state = State::PollContent;
+                    }
+                }
+                State::PollContent => {
+                    return this.writer.poll_write(cx, buf);
+                }
+            }
+        }
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
         todo!()
     }
 
-    pub async fn write(&mut self, buf: &[u8]) -> std::io::Result<()> {
-        let hasher = high_way_hasher();
-        let hash = hasher.hash256(buf);
-        for h in hash {
-            self.writer.write_u64_le(h).await?;
-        }
-        self.writer.write_all(buf).await
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        todo!()
     }
 }
